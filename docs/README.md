09507c64f0c22e14d1253e2b35d2adf1606b16ac
Lurker <434848237@qq.com>
2017-03-16 14:39:17 +0800

    init

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+Code specification document
---
+++ docs/config.js
@@ -0,0 +1,16 @@
+self.$config = {
+    nav: [
+        { title: 'Home', path: '/', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/html.md' },
+        { title: 'Css', path: '/css', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/css.md' },
+        { title: 'Less', path: '/less', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/less.md' },
+        { title: 'Javascript', path: '/javascript', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/javascript.md' },
+        { title: 'ECMAScript', path: '/es', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/es.md' },
+        { title: 'E-Json', path: '/e-json', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/e-json.md' },
+        { title: '模块和加载器', path: '/module', source: 'https://raw.githubusercontent.com/Lurkerc/codeSpec/master/module.md' }
+    ],
+    icons: [{
+        icon: 'github',
+        label: 'Contribute on GitHub',
+        link: 'https://github.com/Lurkerc/codeSpec'
+    }]
+}
